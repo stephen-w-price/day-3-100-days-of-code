900c9b4ee659595e8efbe6e6d68a3a4cefc38c89
Stephen_Price <STEPHEN.WESLEY.PRICE.COM>
2022-04-25 22:35:21 -0400

    lesson completed

---
+++ lesson.py
@@ -0,0 +1,21 @@
+# if condition:
+#     do this 
+# else: 
+#     do this
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry!")
+
+# Comparison Operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to 
+# <= Lesser than or equal to 
+# == Checking to see if it is equal 
+# = Assignment
+# !=
